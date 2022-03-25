41655db8b0383d71030acccc3709b26151081d26
NicMortelliti <npmortelliti@gmail.com>
2022-03-25 16:34:22 -0700

    Add category buttons (not yet functional) to app

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories }) {
+  const btnList = [...categories];
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {btnList.map(eachBtn => (
+        <button key={eachBtn}>{eachBtn}</button>
+      ))}
+      ))
     </div>
   );
 }
